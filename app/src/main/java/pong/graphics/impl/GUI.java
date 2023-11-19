fa291fc418b76d7c765997ecc73421dc8a710f8b
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2023-11-19 18:16:47 +0100

    GUI class 1.0

--- app/src/main/java/pong/graphics/impl/GUI.java
+++ app/src/main/java/pong/graphics/impl/GUI.java
@@ -2,32 +2,32 @@ package pong.graphics.impl;
 
 import javax.swing.*;
 
+import pong.model.impl.GameState;
 import pong.model.impl.World;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
+import java.lang.ModuleLayer.Controller;
 
 public class GUI {
     private JFrame frame;
-    /*private ScenePanel panel;
-    private GameState scene;
-    private Controller controller;*/
+    private PongGamePanel panel;
+    private World scene;
+    private Controller controller;
 
-
-    public GUI(/*World scene,*/ int width, int height){
+    public GUI(World scene, int width, int height) {
         frame = new JFrame("PONG!!");
         frame.setSize(width, height);
         frame.setResizable(false);
-        //this.scene=scene;
-        //frame.serUndecorated(true);
+        this.scene = scene;
+        // frame.serUndecorated(true);
         frame.addWindowListener(new WindowAdapter() {
-            
+
             @Override
             public void windowClosing(WindowEvent ev) {
                 System.exit(0);
             }
-            
+
             @Override
             public void windowClosed(WindowEvent ev) {
                 System.exit(0);
@@ -36,4 +36,19 @@ public class GUI {
         frame.setVisible(true);
     }
 
+
+    /**
+     * Build a new render frame each time is called
+     */
+    public void render(){
+        try {
+			frame.repaint();
+		} catch (Exception ex){
+			ex.printStackTrace();
+		}
+    }
+
+    public void setInputController(Controller c) {
+        controller = c;
+    }
 }
---
+++ app/src/main/java/pong/graphics/impl/PongGamePanel.java
@@ -0,0 +1,53 @@
+package pong.graphics.impl;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+public class PongGamePanel extends JPanel {
+
+    public PongGamePanel(int w, int h){
+        setSize(w, h);
+        this.addKeyListener(new PaddleMover());
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        requestFocusInWindow();
+    }
+
+    @Override 
+    public void paint(Graphics g){
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+        g2.clearRect(0, 0, this.getWidth(), this.getHeight());
+    }
+
+    private class PaddleMover implements KeyListener{
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+        }
+
+    }
+}
