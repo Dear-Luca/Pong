14e873fe4607e819953be4c2bb340161fdec2c5b
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2023-11-19 17:39:42 +0100

    World class built

--- app/src/main/java/pong/model/impl/World.java
+++ app/src/main/java/pong/model/impl/World.java
@@ -1,7 +1,47 @@
 package pong.model.impl;
 
+import java.util.List;
+
+import pong.model.api.GameObject;
+
+import java.util.ArrayList;
+
 public class World {
-    private List<Paddle> padList;
+    private List<Paddel> padList;
     private Ball ball;
     
+    public World(){
+        padList=new ArrayList<Paddel>();
+    }
+
+    public void updateState(long dt) {
+        for (Paddel pad : padList) {
+            pad.updatePosition(dt);
+        }
+
+        ball.updatePosition(dt);
+    }
+
+    public List<GameObject> getSceneEntities(){
+        List<GameObject> entities = new ArrayList<>();
+        entities.addAll(padList);
+        entities.add(ball);
+        return entities;
+    }
+
+    public Paddel getLeftPaddel(){
+        return padList.get(0);
+    }
+
+    public Paddel getRightPaddel(){
+        return padList.get(1);
+    }
+
+    public void setLeftPaddel(Paddel LeftPaddel) {
+        this.padList.set(0,LeftPaddel);
+    }
+
+    public void setRightPaddel(Paddel RightPaddel) {
+        this.padList.set(1,RightPaddel);
+    }
 }
